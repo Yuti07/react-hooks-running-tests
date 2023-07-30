9535172f2db7bbe67363902610057275a50c66b1
Yuti Thakkar <thakkaryuti@gmail.com>
2023-07-30 16:28:07 -0400

    completed tests

--- src/__tests__/Article.test.js
+++ src/__tests__/Article.test.js
@@ -4,8 +4,8 @@ import React from "react";
 
 import Article from "../components/Article";
 
+// src/__tests__/Article.test.js
 test("displays the text 'please pass this test'", () => {
   render(<Article />);
-
   expect(screen.queryByText("please pass this test")).toBeInTheDocument();
 });
